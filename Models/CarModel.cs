97941bf200685a6564ed558067215400420c4b3f
Paul Hart <pmhart@ymail.com>
2016-06-06 10:42:01 -0500

    spelling fixes

--- Models/CarModel.cs
+++ Models/CarModel.cs
@@ -8,7 +8,8 @@ namespace MVVMExample
 		public string[] GetCarNames()
 		{
 
-			return new string[] { "2008 Subaru Legacy", "2011 Ford Fusion", "1995 Chevy Malibu" };
+			//todo: create SQLite car table 
+			return new string[] { "2008 Subaru Legacy", "2011 Ford Fusion", "1995 Chevy Malibu", "2006 Honda Civic" };
 
 		}
 
